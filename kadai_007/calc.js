e13601a6a76c06ed9b14378d271ee08205a15652
licca28 <rtsubaki.0918@gmail.com>
2024-09-24 07:00:48 +0900

    kadai007の提出

---
+++ kadai_007/calc.js
@@ -0,0 +1,21 @@
+let num = Math.floor(Math.random()*100);
+
+// 変数(num)が3と5の倍数の場合： “3と5の倍数です”
+if (num % 15 === 0 ) {
+  console.log('3と5の倍数です');
+}
+
+// 変数(num)が3の倍数の場合： “3の倍数です”
+else if (num % 3 === 0 ) {
+  console.log('3の倍数です');
+}
+
+// 変数(num)が5の倍数の場合： “5の倍数です”
+else if (num % 5 === 0 ) {
+  console.log('5の倍数です');
+}
+
+// それ以外のもの
+else {
+  console.log(num);
+}
