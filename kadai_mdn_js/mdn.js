ecb96a085b540add6d0f3f92848ba3e16462ad99
licca28 <rtsubaki.0918@gmail.com>
2024-09-27 15:05:44 +0900

    kadai_mdnの再提出

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,8 +1,10 @@
 const date = new Date();
-const [month, day, year] = [
-  date.getMonth(),
-  date.getDate(),
-  date.getFullYear(),
-];
 
-console.log(year + '年' + month + '月' + day + '日');
+const options = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
+};
+
+const formattedDate = date.toLocaleDateString('ja-JP', options);
+console.log(formattedDate);
